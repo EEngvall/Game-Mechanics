52ec70ac0348c8473519d075eb0fc1f4e15b0eae
EEngvall <45218590+EEngvall@users.noreply.github.com>
2018-12-19 13:53:51 -0800

    Monster Box Addition
    Added the ability for the function to display a monster name, then add the acquired monster to a list, and later show that list if the user chooses to do so.

--- Gacha_Summon_Test.py
+++ Gacha_Summon_Test.py
@@ -1,27 +1,44 @@
 #Gacha Summon Function Test
 import random
+
+#Function used to generate a summon randomly.  
+#import random was used to decide which summon is selected by assiging a random value between 1-1000 and then selecting a summon based on that value.
+monster_box = []
 def summon_hero():
     summon_chance = (random.randint(1, 1000))
-    mythic_heroes = ['Mythic 1', 'Mythic 2', 'Mythic 3']
+    mythic_heroes = ['', 'Mythic 2', 'Mythic 3']
     legendary_heroes = ['Legendary 1', 'Legendary 2', 'Legendary 3']
     epic_heroes = ['Trash', 'More Trash', 'Epic Trash']
+
     if summon_chance <= 17:
         print('Mythic Summon!')
-        print(mythic_heroes[random.randint(0, len(mythic_heroes)-1)])
+        new_monster = mythic_heroes[random.randint(0, len(mythic_heroes)-1)]
+        print(new_monster)
+        print()
+        monster_box.append(new_monster);
+        print(monster_box)
+        
     elif summon_chance <= 200:
         print('Legendary Summon!')
-        print(legendary_heroes[random.randint(0, len(legendary_heroes)-1)])
+        new_monster = legendary_heroes[random.randint(0, len(legendary_heroes)-1)]
+        print(new_monster)
+        print()
+        monster_box.append(new_monster);
 
     elif summon_chance <= 1000:
         print('Epic Summon!')
-        print(epic_heroes[random.randint(0, len(epic_heroes)-1)])
-
-
-print('Hello Summoner!')
+        new_monster = epic_heroes[random.randint(0, len(epic_heroes)-1)]
+        print(new_monster)
+        print()
+        monster_box.append(new_monster);
+       
+print('****Hello Summoner!****')
 print('How would you like to proceed?\n')
 print('1. Summon a Hero.')
 print('2. Show summon rates.')
-print('3. To Exit')
+print('3. Show Monster Box')
+print('4. Exit')
+
 user_input = input()
 print()
 while user_input != 'Q' or 'q':
@@ -33,8 +50,12 @@ while user_input != 'Q' or 'q':
         print('Legendary Hero: 18.3%')
         print('Legendary Hero: 1.7%')
     elif user_input == '3':
+        print('****Your Monsters****')
+        for items in monster_box:
+            print(items)
+    elif user_input == '4':
         print('Goodbye!')
         break
     
-    user_input = input('Select Choice: \n')
+    user_input = input('Select Choice: ')
     print()
